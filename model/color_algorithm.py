6fdcd1be9a7e4b82429e575bcd2f355f3bc84f58
Nick Tardif <nicktardif@gmail.com>
2023-11-26 15:36:36 -0800

    Bucketing technique

--- model/color_algorithm.py
+++ model/color_algorithm.py
@@ -5,6 +5,7 @@ from model.color_memo import ColorMemo
 
 class ColorAlgorithm(ABC):
     lookup_key: str
+    num_buckets: int
 
     @abstractmethod
     def evaluate(self, percent: float) -> RGB:
@@ -14,16 +15,21 @@ class ColorAlgorithm(ABC):
     def is_reverse(self) -> bool:
         pass
 
+    def get_bucket(self, percent: float) -> int:
+        return math.floor(percent * self.num_buckets)
+
 class RainbowRGB(ColorAlgorithm):
     def __init__(self, offset: float, color_memo: ColorMemo):
         self._offset = offset
         self._count = 0
         self._memo = color_memo
         self.lookup_key = 'RainbowRGBFlow'
+        self.num_buckets = 50
 
     def evaluate(self, percent: float) -> RGB:
-        offset_percent = round((percent + self._offset) % 1.0, 2)
-        precomputed = self._memo.get(self.lookup_key, offset_percent)
+        offset_percent = (percent + self._offset) % 1.0
+        bucket = self.get_bucket(offset_percent)
+        precomputed = self._memo.get(self.lookup_key, bucket)
         if precomputed:
             return precomputed
 
@@ -33,7 +39,7 @@ class RainbowRGB(ColorAlgorithm):
         b = math.sin(a - (4 * math.pi / 3)) * 192 + 128
 
         rgb = RGB(r, g, b)
-        self._memo.set_value(self.lookup_key, offset_percent, rgb)
+        self._memo.set_value(self.lookup_key, bucket, rgb)
         return rgb
 
     def is_reverse(self) -> bool:
